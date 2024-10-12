e831e434655edfd9ee4e163fe283052dc9b0d21e
unknown <cernacek,tomas@gmail,com>
2024-10-12 03:15:53 +0200

    first commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpForce = 5;
+
+    public float Move;
+    public Rigidbody2D rb;
+    private bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            
+        }
+        if(Input.GetButton("Sprint")){
+            speed = 10;
+        }else{
+            speed = 5;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Grass")){
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Grass")){
+            isGrounded = false;
+        }
+    }
+}
---
+++ Assets/ProceduralGeneration.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+
+    [SerializeField] GameObject stonePrefab;
+    [SerializeField] GameObject grassPrefab;
+    [SerializeField] GameObject dirtPrefab;
+    [SerializeField] int width;
+    [SerializeField] int height;
+
+    private int wallSize = 20;
+
+    public void generate(){
+        for(int x = -width; x < width; x++){
+            int minHeigth = height - 1;
+            int maxHeigth = height + 2;
+            height = Random.Range(minHeigth, maxHeigth);
+            int minStoneSpawnDistance = height - 3;
+            int maxStoneSpawnDistance = height - 5;
+            int stoneSpawnDistance = Random.Range(minStoneSpawnDistance, maxStoneSpawnDistance);
+
+            for(int y = 0; y < height; y++){
+                if(y < stoneSpawnDistance){
+                    Instantiate(stonePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                } else {
+                    Instantiate(dirtPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+
+            }
+            Instantiate(grassPrefab, new Vector3(x, height, 0), Quaternion.identity);
+
+            if(x == -width || x == width - 1){
+                for(int y = height + 1; y < height + wallSize; y++){
+                    Instantiate(stonePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                }
+            }
+        }
+    } 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     generate();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
