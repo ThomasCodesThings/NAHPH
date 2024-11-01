769fe571ded860154047137bf403a8e5f4524a9b
ThomasCodesThings <cernacek.tomas@gmail.com>
2024-11-01 15:26:58 +0100

    folder structure

---
+++ Assets/GameStructs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameStructs
+{
+    
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+}
